1073220b3641d09de5617a0fc5f8f9417781f219
Ian Cresse <ijcresse@gmail.com>
2022-06-24 14:52:02 -0700

    lifted input handling to Game, grid now controls grid only

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,23 +5,42 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameObject clock;
-    public GameObject grid;
     public GameObject hintCols;
     public GameObject hintRows;
-    
+    private GridController gridScript;
+    public GameObject cursorPrefab;
+    private CursorController cursorScript;
     
     void Start()
     {
-        // Vector2 topLeft = new Vector2(0, 0);
-        // Instantiate(clock, topLeft, clock.transform.rotation);
-        // Instantiate(grid, topLeft, grid.transform.rotation);
-        // Instantiate(hintCols, topLeft, hintCols.transform.rotation);
-        // Instantiate(hintRows, topLeft, hintRows.transform.rotation);
-        // Instantiate(cursor, gridStart, cursor.transform.rotation);
+        gridScript = GameObject.Find("Grid").GetComponent<GridController>();
+        float boxSize = gridScript.boxSize;
+
+        GameObject cursor = Instantiate(cursorPrefab, gridScript.startPosition, cursorPrefab.transform.rotation);
+        cursor.transform.localScale = new Vector2(boxSize * 2, boxSize * 2);
+        cursorScript = cursor.GetComponent<CursorController>();
     }
 
     void Update()
     {
-        
+        int gameDimensionX = gridScript.gameDimensionX;
+        int gameDimensionY = gridScript.gameDimensionY;
+        float boxSize = gridScript.boxSize;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            cursorScript.Move(gameDimensionX, gameDimensionY, boxSize, xMove: -1);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            cursorScript.Move(gameDimensionX, gameDimensionY, boxSize, xMove: 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            cursorScript.Move(gameDimensionX, gameDimensionY, boxSize, yMove: 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            cursorScript.Move(gameDimensionX, gameDimensionY, boxSize, yMove: -1);
+        }
     }
 }
--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -9,9 +9,7 @@ public class GridController : MonoBehaviour
     private GameObject gridSprite;
     private (float x, float y) gridSpriteSize;
     public float boxSize { get; set; }
-
-    public GameObject cursorPrefab;
-    private CursorController cursorScript;
+    public Vector2 startPosition { get; set; }
     
     void Start()
     {
@@ -19,33 +17,27 @@ public class GridController : MonoBehaviour
         gridSpriteSize = (x: gridSprite.transform.localScale.x, y: gridSprite.transform.localScale.y);
         boxSize = gridSpriteSize.y / gameDimensionY; //always the smaller size. if we wanna do wacky stuff there'll have to be comparison
         Debug.Log($"boxSize: {boxSize}, gridSpriteSize: ({gridSpriteSize.x}, {gridSpriteSize.y})");
-
-        Vector2 startPosition = new Vector2(gridSprite.transform.position.x - (gridSpriteSize.x / 2) + boxSize / 2, 
+        startPosition = new Vector2(gridSprite.transform.position.x - (gridSpriteSize.x / 2) + boxSize / 2, 
                                             gridSprite.transform.position.y + (gridSpriteSize.y / 2) - boxSize / 2);
-
-        GameObject cursor = Instantiate(cursorPrefab, startPosition, cursorPrefab.transform.rotation);
-        cursor.transform.localScale = new Vector2(boxSize * 2, boxSize * 2);
-        cursorScript = cursor.GetComponent<CursorController>();
-
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            cursorScript.Move(gameDimensionX, gameDimensionY, boxSize, xMove: -1);
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            cursorScript.Move(gameDimensionX, gameDimensionY, boxSize, xMove: 1);
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            cursorScript.Move(gameDimensionX, gameDimensionY, boxSize, yMove: 1);
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            cursorScript.Move(gameDimensionX, gameDimensionY, boxSize, yMove: -1);
-        }
+        // if (Input.GetKeyDown(KeyCode.LeftArrow))
+        // {
+        //     cursorScript.Move(gameDimensionX, gameDimensionY, boxSize, xMove: -1);
+        // }
+        // else if (Input.GetKeyDown(KeyCode.RightArrow))
+        // {
+        //     cursorScript.Move(gameDimensionX, gameDimensionY, boxSize, xMove: 1);
+        // }
+        // else if (Input.GetKeyDown(KeyCode.UpArrow))
+        // {
+        //     cursorScript.Move(gameDimensionX, gameDimensionY, boxSize, yMove: 1);
+        // }
+        // else if (Input.GetKeyDown(KeyCode.DownArrow))
+        // {
+        //     cursorScript.Move(gameDimensionX, gameDimensionY, boxSize, yMove: -1);
+        // }
     }
 }
