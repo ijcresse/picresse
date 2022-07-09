3bbc5f1f774692c94eccfd2a654366680de4e977
Ian Cresse <ijcresse@gmail.com>
2022-07-09 14:50:25 -0700

    random puzzle generating and being created properly!

--- Assets/Scripts/PuzzleController.cs
+++ Assets/Scripts/PuzzleController.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class PuzzleController : MonoBehaviour
 {
-    private bool[,] puzzle;
+    public bool[,] puzzle;
     private GameObject[] hintColumns;
     private GameObject[] hintRows;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     public void CreatePuzzle(int x, int y) {
         Debug.Log($"PuzzleController.CreatePuzzle called for puzzle with size {x},{y}");
-        
+        puzzle = new bool[y, x];
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
+                puzzle[i, j] = Random.Range(0f, 1f) <= 0.6f;
+            }
+            string line = "";
+            for (int j = 0; j < x; j++) {
+                line = line + (puzzle[i,j] ? 'o' : 'x');
+            }
+            Debug.Log(line);
+        }
     }
 
     public void CreatePuzzle(string base64Code) {
